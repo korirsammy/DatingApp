a9b0f4432f5ce97e3ad254d20d99df230ab0c442
Korir <sammy.korir@bell.corp.bce.ca>
2018-12-22 02:56:53 -0400

    Added route gurds

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { AlertifyService } from './../_services/alertify.service';
 import { AuthService } from './../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model:any={};
-  constructor(public authService:AuthService, private alertify:AlertifyService) { }
+  constructor(public authService:AuthService, private alertify:AlertifyService,
+     private router:Router) { }
 
   ngOnInit() {
   }
@@ -19,6 +21,8 @@ export class NavComponent implements OnInit {
      this.alertify.success('Logged in Successfully');
    }, error => {
     this.alertify.error(error);
+   },()=> {
+     this.router.navigate(['/members']);
    });
   }
 
@@ -30,6 +34,7 @@ export class NavComponent implements OnInit {
  logOut(){
    localStorage.removeItem('token');
    this.alertify.message('Logged out');
+   this.router.navigate(['/home']);
  }
 
 }
