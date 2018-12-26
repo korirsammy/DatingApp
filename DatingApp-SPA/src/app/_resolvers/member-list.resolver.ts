578511f651d74f09847e86aee32cec8d21323368
Korir <sammy.korir@bell.corp.bce.ca>
2018-12-25 22:53:58 -0400

    Added Paging, Sorting and Filtering

--- DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -1,26 +1,26 @@
-import { AlertifyService } from '../_services/alertify.service';
-import { Injectable } from "@angular/core";
-import { User } from '../_models/user';
-import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import {Injectable} from '@angular/core';
+import {User} from '../_models/user';
+import {Resolve, Router, ActivatedRouteSnapshot} from '@angular/router';
 import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { error } from '@angular/compiler/src/util';
 
 @Injectable()
+export class MemberListResolver implements Resolve<User[]> {
+    pageNumber = 1;
+    pageSize = 5;
 
-export class MemberListResolver implements Resolve<User[]>{
-   
-    constructor(private userService:UserService,
-        private router:Router, private alertify:AlertifyService) {  }      
-    
-        resolve(route:ActivatedRouteSnapshot):Observable<User[]>{
-            return this.userService.getUsers().pipe(
-                catchError(error=>{
-                    this.alertify.error('Error retriving data');
-                    this.router.navigate(['/home']);                    
-                    return of(null)
-                })
-            )
-        }
+    constructor(private userService: UserService, private router: Router,
+        private alertify: AlertifyService) {}
+
+    resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
+        return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
+            catchError(error => {
+                this.alertify.error('Problem retrieving data');
+                this.router.navigate(['/home']);
+                return of(null);
+            })
+        );
+    }
 }
--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -12,7 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
-     [ServiceFilter(typeof(LogUserActivity))]
+    [ServiceFilter(typeof(LogUserActivity))]
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
@@ -20,45 +19,61 @@ namespace DatingApp.API.Controllers
     {
         private readonly IDatingRepository _repo;
         private readonly IMapper _mapper;
+
         public UsersController(IDatingRepository repo, IMapper mapper)
         {
             _mapper = mapper;
             _repo = repo;
-
         }
+
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
+        public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _repo.GetUsers();
-            var usersToReturn=_mapper.Map<IEnumerable<UserForListDto>>(users);
-            return Ok(usersToReturn);
+            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
 
+            var userFromRepo = await _repo.GetUser(currentUserId);
+
+            userParams.UserId = currentUserId;
+
+            if (string.IsNullOrEmpty(userParams.Gender))
+            {
+                userParams.Gender = userFromRepo.Gender == "male" ? "female" : "male";
+            }
+
+            var users = await _repo.GetUsers(userParams);
+
+            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            Response.AddPagination(users.CurrentPage, users.PageSize,
+                users.TotalCount, users.TotalPages);
+
+            return Ok(usersToReturn);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetUser")]
         public async Task<IActionResult> GetUser(int id)
         {
             var user = await _repo.GetUser(id);
-            var userToReturn=_mapper.Map<UserForDetailsDto>(user);
+
+            var userToReturn = _mapper.Map<UserForDetailsDto>(user);
+
             return Ok(userToReturn);
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
         {
-            if(id!=int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
 
-            var userFromRepo= await _repo.GetUser(id);
+            var userFromRepo = await _repo.GetUser(id);
 
-            Mapper.Map(userForUpdateDto,userFromRepo);
-            if(await _repo.SaveAll())
-               return NoContent();
-            
-            throw new Exception($"Updating user with {id} failed on update");
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await _repo.SaveAll())
+                return NoContent();
+
+            throw new Exception($"Updating user {id} failed on save");
         }
-
     }
-
-
 }
